187868dbaa25415bb3f6b4ac1937d27e86a96904
Xolmatov1156 <xolmatov1156@gmail.com>
2024-10-19 18:25:30 +0500

    init

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const New = () => {
   return (
-    <div className='w-[1370px]'>New</div>
+    <div className='w-[1370px] mx-auto'>
+      <h2 className='text-center text-[45px] font-semibold'>Новости</h2>
+      <p className='text-gray-500 font-thin text-center'>Bukhara Natural Product</p>
+    </div>
   )
 }
 
